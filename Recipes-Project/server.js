2feef37722cc8add4432c8450f729d3568d89161
EladLaster <eladlaster1@walla.com>
2025-09-12 13:16:38 +0300

    Fixes for deployment

--- Recipes-Project/server.js
+++ Recipes-Project/server.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
 const express = require("express");
 const morgan = require("morgan");
 const { errorHandling } = require("./middlewares/errorHandling");
@@ -5,12 +9,15 @@ const recipeRoute = require("./routes/recipeRoute");
 const userRoute = require("./routes/userRoute");
 const userFavoriteRoute = require("./routes/userFavoriteRoute");
 const app = express();
-const PORT = 3030;
 
 app.use(express.json());
 
 morgan.token("date", () => new Date().toISOString());
 app.use(morgan(":method :url :status :response-time ms - :date"));
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
 
 app.use('/auth', userRoute)
 
@@ -21,5 +28,5 @@ app.use('/api/recipes', recipeRoute);
 app.use(errorHandling);
 
 app.listen(PORT,()=>{
-    console.log(`server run on port: ${PORT}`);
+    console.log(`server running on port: ${PORT}`);
 })
